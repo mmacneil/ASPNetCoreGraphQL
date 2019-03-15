7b31d159bb67a6b8a2b50dda32c41a7e7ebf1e3d
Mark Macneil <markmacneil@gmail.com>
2019-03-15 11:07:57 -0300

    Update ContextServiceLocator.cs
    Indentation

--- src/backend/NHLStats.Api/Helpers/ContextServiceLocator.cs
+++ src/backend/NHLStats.Api/Helpers/ContextServiceLocator.cs
@@ -5,7 +5,7 @@ using NHLStats.Core.Data;
 
 namespace NHLStats.Api.Helpers
 {
-	// https://github.com/graphql-dotnet/graphql-dotnet/issues/648#issuecomment-431489339
+    // https://github.com/graphql-dotnet/graphql-dotnet/issues/648#issuecomment-431489339
     public class ContextServiceLocator
     {
         public IPlayerRepository PlayerRepository => _httpContextAccessor.HttpContext.RequestServices.GetRequiredService<IPlayerRepository>();
